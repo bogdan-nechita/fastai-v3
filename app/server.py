a3b31d86e725707eeab50ad2919112fbdba6cc87
Bogdan Nechita <bogdan.nechita@qgcatalyst.com>
2019-05-29 16:05:02 +0300

    Add a log for download start.

--- app/server.py
+++ app/server.py
@@ -31,6 +31,7 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
+    print('Start downloading model.')
     await download_file(export_file_url, path / export_file_name)
     try:
         learn = load_learner(path, export_file_name)
