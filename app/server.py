6083d16b60a221ab2147e2e5d1fddc528b757ca7
Bogdan Nechita <bogdan.nechita@qgcatalyst.com>
2019-06-06 10:08:42 +0300

    Separate the endpoints to use different models for one and two pads.

--- app/server.py
+++ app/server.py
@@ -9,11 +9,22 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1KpYllvvVCLiXRsCck14aXn-O808ZKA8r'
-export_file_name = 'resnet50_81_acc_29_may_2019.pkl'
 
-classes = ['depleted', 'low', 'threshold', 'target', 'high', 'two_pad_high_high', 'two_pad_high_low', 'two_pad_low_low']
+export_one_pad_file_url = 'https://drive.google.com/uc?export=download&id=1KpYllvvVCLiXRsCck14aXn-O808ZKA8r'
+export_one_pad_file_name = 'resnet50_81_acc_29_may_2019.pkl'
+
+export_two_pads_file_url = 'https://drive.google.com/uc?export=download&id=1KpYllvvVCLiXRsCck14aXn-O808ZKA8r'
+export_two_pads_file_name = 'resnet50_81_acc_29_may_2019.pkl'
+
+
+# classes = ['depleted', 'low', 'threshold', 'target', 'high', 'two_pad_high_high', 'two_pad_high_low', 'two_pad_low_low']
+# one_pad_classes = ['depleted', 'low', 'threshold', 'target', 'high']
 path = Path(__file__).parent
+
+models_path = path / "models"
+
+one_pad_model_path = models_path / "one_pad_model"
+two_pads_model_path = models_path / "two_pads_model"
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
@@ -22,6 +33,7 @@ app.mount('/static', StaticFiles(directory='app/static'))
 
 async def download_file(url, dest):
     if dest.exists():
+        print("File already exists.")
         return
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
@@ -30,11 +42,26 @@ async def download_file(url, dest):
                 f.write(data)
 
 
-async def setup_learner():
-    print('Start downloading model.')
-    await download_file(export_file_url, path / export_file_name)
+async def setup_one_pad_learner():
+    print('Start downloading one pad model.')
+    await download_file(export_one_pad_file_url, one_pad_model_path / export_one_pad_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner(one_pad_model_path, export_one_pad_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+async def setup_two_pad_learner():
+    print('Start downloading two pad model.')
+    await download_file(export_two_pads_file_url, two_pads_model_path / export_two_pads_file_name)
+    try:
+        learn = load_learner(two_pads_model_path, export_two_pads_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
@@ -46,8 +73,15 @@ async def setup_learner():
 
 
 loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+
+tasks = [asyncio.ensure_future(setup_one_pad_learner())]
+
+one_pad_model = loop.run_until_complete(asyncio.gather(*tasks))[0]
+
+tasks = [asyncio.ensure_future(setup_two_pad_learner())]
+
+two_pad_model = loop.run_until_complete(asyncio.gather(*tasks))[0]
+
 loop.close()
 
 
@@ -57,12 +91,23 @@ async def homepage(request):
     return HTMLResponse(html_file.open().read())
 
 
-@app.route('/analyze', methods=['POST'])
-async def analyze(request):
+@app.route('/analyze_two_pads', methods=['POST'])
+async def analyze_two_pads(request):
+    print('analyze_two_pads call')
     img_data = await request.form()
     img_bytes = await(img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
+    prediction = two_pad_model.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+
+@app.route('/analyze_one_pad', methods=['POST'])
+async def analyze_one_pad(request):
+    print('analyze_one_pad call')
+    img_data = await request.form()
+    img_bytes = await(img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = one_pad_model.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
 
